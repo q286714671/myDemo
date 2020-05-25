f799b2973a98d457f4b894586952f8be6976ce42
yongqi <286714671@qq.com>
2020-05-25 21:59:51 +0800

    测试提交

---
+++ app/src/main/java/com/example/yongqi/demo/testkt/TestKT.kt
@@ -0,0 +1,23 @@
+package com.example.yongqi.demo.testkt
+
+import android.content.Context
+import android.widget.Toast
+import java.sql.DriverManager.println
+
+var age = 18
+var name:String = "xiaoming"
+
+fun echo(context: Context,msg:String){
+    Toast.makeText(context,msg,Toast.LENGTH_LONG).show()
+
+}
+
+fun main(arg:Array<String>){
+    A.a.putNum(11)
+
+    println("")
+
+   
+
+
+}
